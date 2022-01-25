fc3e0460861366da3daa588dac673c05d5c0dfdb
An Phan <anphan@Ans-MacBook-Pro.local>
2022-01-24 22:25:21 -0600

    added buttons for main and repo list, starting on css

--- components/RepoList.js
+++ components/RepoList.js
@@ -1,35 +1,46 @@
 import { useState, useEffect } from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
+const _ = require("lodash");
 
 export default function RepoList(props) {
-  
+
     const [repoList, setRepoList] = useState(null)
     const [isLoading, setLoading] = useState(false)
-  
-    useEffect(() => {
-      setLoading(true)
-      fetch(`https://api.github.com/users/${props.username}/repos`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data)
-            setRepoList(data)
-            setLoading(false)
 
-          })
+    useEffect(() => {
+        setLoading(true)
+        fetch(`https://api.github.com/users/${props.username}/repos`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                setRepoList(data)
+                setLoading(false)
+
+            })
 
     }, [])
-  
-    if (isLoading) return <p>Loading...</p>
-    if (!repoList) return <p>No repo data</p>
-  
-    console.log(repoList);
 
-  return (
-    <div>
-      <div className="header">
-    {repoList.map((repo) => (
-        <div className="repo" key={repo.id}>{repo.name}</div>
-    ))}
-      </div>
-    </div>
-  )
+    if (isLoading) return <ClipLoader loading={isLoading} size={150} />
+    if (!repoList) return <p>No repo data</p>
+
+    console.log(repoList);
+    const orderedList = _.orderBy(repoList, ['stargazers_count'], ['desc']);
+
+    return (
+        <div>
+            <div className="header">
+                {orderedList.map((repo) => (
+                    <div className="repo" key={repo.id}>
+                        <h2>{repo.name}</h2>
+                        <p></p>
+                        <p>{repo.stargazers_count}</p>
+
+
+
+                    </div>
+
+                ))}
+            </div>
+        </div>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,55 @@
 import { useState, useEffect } from 'react';
 import RepoList from '../components/RepoList';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import ClipLoader from "react-spinners/ClipLoader";
+
 
 const WORDPRESS_API_GITHUB_USER_URL = "https://zogo-7a8a1d.ingress-erytho.easywp.com/wp-json/wp/v2/pages/2";
 
 export default function Home() {
-  
-    const [githubUser, setGithubUser] = useState(null)
-    const [isLoading, setLoading] = useState(false)
-  
-    useEffect(() => {
-      setLoading(true)
-      fetch(WORDPRESS_API_GITHUB_USER_URL)
-        .then((res) => res.json())
-        .then((wpData) => {
- 
-          fetch(`https://api.github.com/users/${wpData.acf.user}`)
+
+  const [githubUser, setGithubUser] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+  const [activeTab, setActiveTab] = useState('overview')
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(WORDPRESS_API_GITHUB_USER_URL)
+      .then((res) => res.json())
+      .then((wpData) => {
+
+        fetch(`https://api.github.com/users/${wpData.acf.user}`)
           .then((res) => res.json())
           .then((ghData) => {
             setGithubUser(ghData)
             setLoading(false)
           })
 
-        })
-    }, [])
-  
-    if (isLoading) return <p>Loading...</p>
-    if (!githubUser) return <p>No User data</p>
-  
+      })
+  }, [])
+
+  if (isLoading) return <ClipLoader loading={isLoading} size={150} />
+
+  if (!githubUser) return <p>No User data</p>
+
   console.log(githubUser);
   return (
     <div className={styles.container}>
       <div className="header">
-      <h1>{githubUser.login}</h1>
-      <img src={githubUser.avatar_url}/>
-      <p>{githubUser.location}</p>
+      <button onClick={() => setActiveTab('overview')}>Overview</button>
+      <button onClick={() => setActiveTab('repolist')}>Repos</button>
 
-      <RepoList username={githubUser.login} />
+        {activeTab === 'overview' &&
+          <>
+            <h1>{githubUser.login}</h1>
+            <img className="userimg" src={githubUser.avatar_url} />
+            <p>{githubUser.location}</p>
+            <p>{githubUser.bio}</p>
+
+          </>}
+        {activeTab === 'repolist' &&
+          <RepoList username={githubUser.login} />
+        }
 
       </div>
     </div>
